cfc78d4ce2a18165639da4b4ae4ded735c2eed64
Christopher <chrisjjcode@gmail.com>
2016-01-23 21:36:17 +0000

    filter now seperates visp and update db

--- editFilter.php
+++ editFilter.php
@@ -2,39 +2,8 @@
 require_once('connect.php');
 require_once('pools.php');
 
-ini_set('memory_limit', '1024M');
-
-$PNpools = [];
-$PNamberKpi = [];
-$PNredKpi = [];
-
-$JLPpools = [];
-$JLPamberKpi = [];
-$JLPredKpi = [];
-
-$filter = '';
-
-foreach($_POST as $key => $value){
-    
-    if(strstr($key, 'visp-pn') == 0)
-    {
-        $PNpools[] = $key;
-    }
-    else
-    {
-        $JLPpools[] = $key;
-    }
-}
-
-print_r($_POST);
-
-for($i = 0; $i < count($_POST); $i++)
-{
-    if($i % 3 == 0)
-    {
-        $PNpools[] = $_POST['filters'][0];
-    }
-}
+$filter = array_keys($_POST);
+$filter = $filter[0];
 
 $array = json_decode($_POST['filters']);
 
@@ -58,38 +27,64 @@ for($i = 0; $i < count($array); $i++)
     $counter++;
 }
 
-var_dump($filters);
+$PNpools = [];
+$PNamberKpi = [];
+$PNredKpi = [];
 
-//$conn->query('TRUNCATE TABLE ' . $filter . 'Filter');
+$JLPpools = [];
+$JLPamberKpi = [];
+$JLPredKpi = [];
 
-//for($i = 0; $i < count($PNpools); $i++)
-//{
-    //$sql = "INSERT INTO ".$filter."Filter (visp, pool, amber_kpi, red_kpi) 
-    //VALUES('PN', '".$PNpools[$i]."', '".$PNamberKpi[$i]."', '".$PNredKpi[$i]."')";
+for($i = 0; $i < count($filters['pool']); $i++)
+{
+    if(strpos($filters['pool'][$i], 'pn') !== false)
+    {
+        $PNpools[] = str_replace("visp-pn-", "", $filters['pool'][$i]);
+        $PNamberKpi[] = $filters['amber-kpi'][$i];
+        $PNredKpi[] = $filters['red-kpi'][$i];
+    }
+    elseif(strpos($filters['pool'][$i], 'jlp') !== false)
+    {
+        $JLPpools[] = str_replace("visp-jlp-", "", $filters['pool'][$i]);
+        $JLPamberKpi[] = $filters['amber-kpi'][$i];
+        $JLPredKpi[] = $filters['red-kpi'][$i];
+    }
+}
+
+var_dump($JLPpools);
+var_dump($JLPamberKpi);
+var_dump($JLPredKpi);
+
+$conn->query('TRUNCATE TABLE ' . $filter . 'Filter');
+
+for($i = 0; $i < count($PNpools); $i++)
+{
+    $sql = "INSERT INTO ".$filter."Filter (visp, pool, amber_kpi, red_kpi) 
+    VALUES('PN', '".$PNpools[$i]."', '".$PNamberKpi[$i]."', '".$PNredKpi[$i]."')";
     
-    //if ($conn->query($sql) === TRUE) 
-    //{
-        //echo "New record created successfully";
-    //} else 
-    //{
-        //echo "Error: " . $sql . "<br>" . $conn->error;
-    //}
+    if ($conn->query($sql) === TRUE) 
+    {
+        echo "New record created successfully";
+    } else 
+    {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
 
-//}
+}
 
-//for($i = 0; $i < count($JLPpools); $i++)
-//{
-    //$sql = "INSERT INTO ".$filter."Filter (visp, pool, amber_kpi, red_kpi) 
-    //VALUES('JLP', '".$JLPpools[$i]."', '".$JLPamberKpi[$i]."', '".$JLPredKpi[$i]."')";
+for($i = 0; $i < count($JLPpools); $i++)
+{
+    $sql = "INSERT INTO ".$filter."Filter (visp, pool, amber_kpi, red_kpi) 
+    VALUES('JLP', '".$JLPpools[$i]."', '".$JLPamberKpi[$i]."', '".$JLPredKpi[$i]."')";
     
-    //if ($conn->query($sql) === TRUE) 
-    //{
-        //echo "New record created successfully";
-    //} else 
-    //{
-        //echo "Error: " . $sql . "<br>" . $conn->error;
-    //}
+    if ($conn->query($sql) === TRUE) 
+    {
+        echo "New record created successfully";
+    } else 
+    {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
 
-//}
+}
 
-//$conn->close();
+$conn->close();
